b2133b88155f685a14728bfe74b43d097a88b689
lesseradmin <veeman961@gmail.com>
2026-07-31 18:15:37 -0500

    Smooth the intro lede after the equalizer line.
    Drop the awkward colon and make democratizing open a path
    instead of equating it to one.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const BUILD_VERSION = '20260731.11';
+  const BUILD_VERSION = '20260731.12';
   const DATA_ROOT = new URL('./data/', document.baseURI);
   const THEME_KEY = 'boplog-theme';
   const ACTIVITY_MODE_KEY = 'boplog-activity-mode';
